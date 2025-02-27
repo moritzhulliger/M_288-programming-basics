938f997a565d0f5819c685e275b25ec493d28436
Max Hoeres <Xx_FortniteSujukPr__xXol@email.com>
2025-02-27 09:07:15 +0100

    test

--- aufgaben/basics/aufgabe2.js
+++ aufgaben/basics/aufgabe2.js
@@ -3,5 +3,22 @@
     Erstellt dazu einen String `helloString` und einen String `yourName` und kombiniert diese.
 **/ 
 
-
-
+const helloString = "Hallo";
+ 
+const yourName = "Max Hoeres";
+ 
+let helloText = + " " + yourName + "!!!!";
+ 
+console. log(helloString, yourName)
+ 
+let age = 12;
+ 
+let ageText = "Du bist" + age + " Jahre alt "
+ 
+console.log(helloText + ageText)
+ 
+console.log("Ein Jahr vergeht .... die Zeit läuft ...")
+ 
+age = age + 1;
+ 
+console.log("Du bist jetzt " + age + " Jahre alt")
