b78ced4ad9b94efd2dcfe859fa41246d03f9f300
Melina Schulze <melina.schulze@bbzsogr.ch>
2025-02-27 17:03:23 +0100

    Aufgaben

--- aufgaben/basics/aufgabe3.js
+++ aufgaben/basics/aufgabe3.js
@@ -5,3 +5,33 @@
  * 
  * Tipp: https://www.w3schools.com/js/js_array_iteration.asp
  */
+
+const JanaMueller = {
+    vorname: "Jana ",
+    nachname: "Mueller",
+    alter: "17",
+    beruf: "Arzt"
+}
+
+const RobertSteiner = {
+    vorname: "Robert ",
+    nachname: "Steiner",
+    alter: "32",
+    beruf: "Informatiker"
+}
+
+const LeaJaengi = {
+    vorname: "Lea ",
+    nachname: "Jaengi",
+    alter: "20",
+    beruf: "Feuerwehrfrau"
+}
+
+
+let ListePersonen = [JanaMueller, RobertSteiner, LeaJaengi];
+
+ListePersonen.forEach(sayHello);
+
+function sayHello(value) {
+    console.log("Hallo " + value.vorname + value.nachname + " du bist " + value.alter + " Jahre alt und arbeitest als " + value.beruf + ".")
+}
