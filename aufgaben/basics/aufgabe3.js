3b3cf9f5327bb159016e01c29e325d4d805ee5b2
Mona Lisa <benbobbobl@email.com>
2025-02-27 17:07:23 +0100

    ffgff

--- aufgaben/basics/aufgabe3.js
+++ aufgaben/basics/aufgabe3.js
@@ -5,3 +5,16 @@
  * 
  * Tipp: https://www.w3schools.com/js/js_array_iteration.asp
  */
+
+const heidi = {name:"Heidi", alter:"80", beruf:"Informatiker"};
+const nik = {name:"nik", alter:"23", beruf:"Kaufmann"};
+const Lukas = {name:"Lukas", alter:"15", beruf:"rennfahrer"}
+
+
+const persons = [heidi, nik, Lukas];
+
+persons.forEach(greetings);
+
+function greetings(pers, index, array) {
+  console.log("Hallo " + pers.name +  "du bist" + pers.alter +  "alt" + "und arbeitest als" + pers.beruf )
+}
