6f6eba71382f17f380acf12a86b30c468cba0a46
Jasmin Gerber <jasmin.gerber@bbzsogr.ch>
2025-02-27 09:59:09 +0100

    etst

--- demos/02_data-structures/array.js
+++ demos/02_data-structures/array.js
@@ -1,9 +1,10 @@
-let array = ['Hallo', 'Velo'];
+let fahrzeuge = ['Bus', 'Velo', 'Auto', 'Zug', 'Tram'];
 
-console.log(`Array ${array}`);
+console.log(`Array ${fahrzeuge}`);
+console.log('Array' + fahrzeuge);
 
-console.log(`Array erstes Element ${array[0]}`)
+console.log(`Array erstes Element ${fahrzeuge[0]}`)
 
-console.log(`Array zweites Element ${array[1]}`)
+console.log(`Array zweites Element ${fahrzeuge[1]}`)
 
-console.log(`Array nicht vorhandenes Element ${array[6]}`)
+console.log(`Array nicht vorhandenes Element ${fahrzeuge[6]}`)
