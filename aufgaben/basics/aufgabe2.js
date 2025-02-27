91c78bc6041b499f0eb230849172860350f1a657
Mischa Siegrist <mischa.sigi@gmail.com>
2025-02-27 09:07:28 +0100

    ig bi cool

--- aufgaben/basics/aufgabe2.js
+++ aufgaben/basics/aufgabe2.js
@@ -3,5 +3,12 @@
     Erstellt dazu einen String `helloString` und einen String `yourName` und kombiniert diese.
 **/ 
 
-
-
+const helloString = "Hallo";
+const yourName = "Mischa";
+const helloText = helloString + " " + yourName + " !!! ";
+let age = 20;
+let ageText = "Du bist " + age + " Jahre alt";
+console.log(helloText + ageText);
+console.log("Ein Jahr vergeht .... die Zeit läuft ...");
+age = age + 1;
+console.log("Du bist jetzt " + age + "Jahre alt");
--- aufgaben/basics/aufgabe3.js
+++ aufgaben/basics/aufgabe3.js
@@ -2,4 +2,11 @@
  * Erstellt ein Object für eine Person, diese Person soll einen Namen, ein Alter und ein Beruf haben.
  * Dann gebt folgenden String aus: `Hallo 'name', du bist 'alter' Jahre alt und arbeitest als 'beruf'`.
  */
+const person = {
+    name: "Mischa",
+    alter: 20,
+    beruf: "Mediamatiker"
+};
 
+const greeting = `Hallo ${person.name}, du bist ${person.alter} Jahre alt und arbeitest als ${person.beruf}.`;
+console.log(greeting);
