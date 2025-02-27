7a9ac6c2d382deca97606fe14564168419214b7d
Moritz <moritz.hulliger@gmail.com>
2025-02-27 08:47:19 +0100

    aufgaben update

--- aufgaben/basics/aufgabe2.js
+++ aufgaben/basics/aufgabe2.js
@@ -1,14 +1,17 @@
 /*** 
     Schreibe ein Programm, welches Hallo DeinName, in die console schreibt.
     Erstellt dazu einen Variable `helloString` und einen String `yourName` und kombiniert diese.
+
+    https://www.w3schools.com/js/js_strings.asp
+    https://www.w3schools.com/js/js_numbers.asp
 **/
 "use strict";
 
-let helloString = "Hallo";
+const helloString = "Hallo";
 
-let yourName = "Moritz Dao";
+const yourName = "Moritz Dao";
 
-let helloText = helloString + " " + yourName + "!!!! ";
+const helloText = helloString + " " + yourName + "!!!! ";
 
 let age = 37;
 
