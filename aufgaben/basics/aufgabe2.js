d434e803b531094cd1ce8f8d3623b14926980476
Mona Lisa <benbobbobl@email.com>
2025-02-27 09:07:23 +0100

    Arbeit an aufgabe 2

--- aufgaben/basics/aufgabe2.js
+++ aufgaben/basics/aufgabe2.js
@@ -1,7 +1,21 @@
 /*** 
     Schreibe ein Programm, welches Hallo DeinName, in die console schreibt.
-    Erstellt dazu einen String `helloString` und einen String `yourName` und kombiniert diese.
+    Erstellt dazu einen Variable `helloString` und einen String `yourName` und kombiniert diese.
 **/ 
+let helloString = "Hallo"
 
+let yourName ="Ben46"
 
+let hellotext = helloString + " " + yourName + "!!!!"
 
+let age = 46;
+
+let ageText = "Du bist" + age + "Jahre alt"
+
+console.log(hellotext + ageText)
+
+console.log("ein jahr vergeht ..... die zeit läuft")
+
+age = age + 1;
+
+console.log("du bist jetzt" + age + " Jahre alt")
