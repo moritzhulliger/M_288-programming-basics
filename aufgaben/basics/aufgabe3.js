7708d619e41ca1327766f3979ffe56a6f094f68c
Samil Ceylan <samil_ceylan96@hotmail.com>
2025-02-27 17:08:13 +0100

    update

--- aufgaben/basics/aufgabe3.js
+++ aufgaben/basics/aufgabe3.js
@@ -5,3 +5,38 @@
  * 
  * Tipp: https://www.w3schools.com/js/js_array_iteration.asp
  */
+const JoergBrunner = {
+    vorname: "Joerg ",
+    nachname: "Brunner",
+    alter: "45 ",
+    beruf: " Polizist"
+}
+
+const LukasWaelti = {
+    vorname: "Lukas ",
+    nachname: "Waelti",
+    alter: "25 ",
+    beruf: " Strassenbauer"
+}
+
+const LeaHochstrasser = {
+    vorname: "Lea ",
+    nachname: "Hochstrasser",
+    alter: "23 ",
+    beruf: " Malerin"
+}
+
+const KarinMeier = {
+    vorname: "Karin ",
+    nachname: "Meier",
+    alter: "34 ",
+    beruf: " Koechin"
+}
+
+let listePersonen = [JoergBrunner, LukasWaelti, LeaHochstrasser, KarinMeier];
+
+listePersonen.forEach(greetPerson);
+
+function greetPerson(value) {
+    console.log("Hallo " + value.vorname + value.nachname + " du bist " + value.alter + "Jahre alt und arbeitest als" + value.beruf + ".")
+}
--- demos/demo.js
+++ demos/demo.js
@@ -12,3 +12,7 @@ console.log(inputVomKunden);
 if(inputVomKunden === "Hallo") {
     alert("Velo")
 }
+
+let fahrzeuge = ["Auto", "Velo" , "Flugzeug", "Schiff"];
+console.log("Array" + fahrzeuge);
+console.log(Array erstes Element ${fahrzeuge[0]})
