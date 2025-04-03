bee55c7f17ed78d26bebe67cf68ebf84eb4e416e
Mischa Siegrist <mischa.sigi@gmail.com>
2025-04-03 09:22:07 +0200

    k

--- aufgaben/drawing/aufgabe1.js
+++ aufgaben/drawing/aufgabe1.js
@@ -10,3 +10,21 @@ this.canvas = document.getElementById('myCanvas');
 if (!this.canvas) throw new Error("Canvas not found");
 
 const ctx = this.canvas.getContext('2d');
+ctx.moveTo(0, 0);
+ctx.lineTo(400, 200);
+ctx.stroke();
+
+drawSun(200, 50, '#FFDF22', 40);
+drawSun(500, 300, '#FFDF22', 60);
+
+function drawSun(positionX, positionY, color, size) {
+    ctx.beginPath();
+ctx.arc(positionX, positionY, size, 0, 2 * Math.PI);
+ctx.fillStyle = color;
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(0, 400, 800, 800);
+ctx.fillStyle = '#40531B';
+ctx.fillRect(0, 400, 800, 800);
+}
